a5eb981df89b8ab01c82649def4961fa95ba2b2e
LiDuan Xu <liduan.xu@jyquant.com.cn>
2022-02-17 16:19:03 +0800

    add webpack remove-comments-plugin

--- remove-comments-plugin.js
+++ remove-comments-plugin.js
@@ -1,13 +1,23 @@
 class RemoveCommentsPlugin {
-    apply(compiler) {
-        console.log('RemoveCommentsPlugin启动');
-        console.log('compiler', compiler);
-        compiler.emit.tap('RemoveCommentsPlugin', compilation => {
-            for(const name in compilation.assets) {
-                console.log(name);
-            }
-        })
-    }
+  apply(compiler) {
+    console.log("RemoveCommentsPlugin启动");
+    console.log("compiler", compiler.plugin);
+    compiler.plugin("emit", (compilation, callback) => {
+      console.log("callBack", callback);
+      for (const name in compilation.assets) {
+        console.log("name-------------------", name);
+        if (name.endsWith(".js")) {
+          const contents = compilation.assets[name].source();
+          const noComments = contents.replace(/\/\*{2,}\/\s?/g, "");
+          compilation.assets[name] = {
+            source: () => noComments,
+            size: () => noComments.length
+          };
+        }
+      }
+      callback();
+    });
+  }
 }
 
-module.exports = RemoveCommentsPlugin
+module.exports = RemoveCommentsPlugin;
